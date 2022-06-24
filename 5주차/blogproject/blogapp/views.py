880f3ed43256ccd2be5d31636e6cce71e05755ce
hye0e <nanda5050@naver.com>
2022-06-24 21:16:22 +0900

    [FEAT] 5주차 django form을 이용한 사용자 입력받기

--- 5주차/blogproject/blogapp/views.py
+++ 5주차/blogproject/blogapp/views.py
@@ -1,5 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Blog
+from django.utils import timezone
+from .forms import BlogForm
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+# 블로그 글 작성 html을 보여주는 함수
+def new(request):
+    return render(request, 'new.html')
+
+# create 는 실제로 글이 만들어지는 함수
+def create(request):
+    if(request.method == 'POST'):
+        # 객체 생성
+        post = Blog()
+        post.title = request.POST['title']
+        post.body = request.POST['body']
+        post.date = timezone.now()
+        # 저장
+        post.save()
+    return redirect('home')
+
+# django form 을 이용해서 입력값을 받는 함수
+# GET 요청 (= 입력 값을 받을 수 있는 html을 갖다 줘야함)
+# POST 요청 (= 입력한 내용을 데이터베이스에 저장하는 기능을 수행 즉, form에서 입력한 내용을 처리 )
+# 둘 다 처리가 가능한 함수
+def formcreate(request): 
+    if request.method == 'POST':
+        # 입력 내용을 DB에 저장
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            # 저장로직
+            post = Blog()
+            post.title = form.cleaned_data['title']
+            post.body = form.cleaned_data['body']
+            post.save()
+            return redirect('home')
+    else: 
+        # 입력을 받을 수 있는 html을 갖다주기
+        form = BlogForm()
+    # 세번째 인자는 딕셔너리 자료형으로 넘겨주어야함
+    return render(request, 'form_create.html', {'form': form})
--- 5주차/blogproject/blogproject/urls.py
+++ 5주차/blogproject/blogproject/urls.py
@@ -19,5 +19,12 @@ from blogapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name = 'home')
+    path('', views.home, name = 'home'),
+
+    # html form을 이용해 블로그 객체 만들기
+    path('new/', views.new, name = 'new'),
+    path('create/', views.create, name = 'create'),
+
+    # django form을 이용해 블로그 객체 만들기
+    path('formcreate/'. views.formcreate, name = 'formcreate')
 ]
